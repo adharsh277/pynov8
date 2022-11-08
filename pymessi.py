065e4f96beb77b2ded8e5749ed787b31809f053c
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-11-08 17:15:34 +0530

    Add files via upload

---
+++ pymessi.py
@@ -0,0 +1,53 @@
+thisdict1 = {
+    'brand': 'greash yeagaer',
+    'model': 'eren',
+    'year': 24367
+}
+print(thisdict1)
+print(len(thisdict1))
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict)
+# %35
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict["brand"])
+print(thisdict["year"])
+print(thisdict["brand"])
+# 234
+thisdict2 = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "year": 2020
+}
+print(thisdict)
+print(len(thisdict2))
+
+thisdict3 = {
+    "brand": "Ford",
+    "electric": False,
+    "year": 1964,
+    "colors": ["red", "white", "blue"]
+}
+print(thisdict3)
+print(type(thisdict3))
+# accesing items
+thisdict = {
+    'brand': "ford",
+    'model': 'mustang',
+    'year': '1936'
+}
+z = thisdict['model']
+y = thisdict['brand']
+x = thisdict['year']
+print(x)
+print(y)
+print(z)
